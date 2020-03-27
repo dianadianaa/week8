79a0e75bf2a25c5e6c463a22f9d361ee57c6e30a
dianadianaa <47651857+dianadianaa@users.noreply.github.com>
2020-03-27 09:19:52 +0600

    Add files via upload

---
+++ 2c.py
@@ -0,0 +1,7 @@
+a= int(input("a:"))
+b= int(input("b:"))
+if(a!=1 and b!=1 or a==1 and b==1):
+	print("YES")
+else:
+	print("NO")
+
---
+++ 2e.py
@@ -0,0 +1,8 @@
+a= int(input("a:"))
+b= int(input("b:"))
+if(a>b):
+	print("1")
+elif(b>a):
+	print("2")
+else:
+	print("0")
